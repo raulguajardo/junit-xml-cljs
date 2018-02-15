beef5c960b720feee223d4325ae51b77e13c4eba
DESKTOP-POTLDLH\rguaj <rguajardo95@gmail.com>
2018-02-15 18:49:07 -0500

    Initial commit

---
+++ test/junit_xml_cljs/core_test.clj
@@ -0,0 +1,7 @@
+(ns junit-xml-cljs.core-test
+  (:require [clojure.test :refer :all]
+            [junit-xml-cljs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
